bfb30b59e60c1090782ba700352eea1121393f0b
libing1 <18317776637@163.com>
2019-06-05 23:00:42 +0800

    家中提交

--- node-study/node-img/index.js
+++ node-study/node-img/index.js
@@ -25,7 +25,7 @@ var server = app.listen(8091, 'lb.renren.com' , function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    // console.log("应用实例，访问地址为 http://%s:%s", host, port)
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
 });
 
 
