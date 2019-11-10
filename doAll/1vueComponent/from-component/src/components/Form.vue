c8b39e6baa57528e1e2ff596dd6f2cebada36350
libing1 <18317776637@163.com>
2019-11-10 21:32:15 +0800

    home 提交

--- doAll/1vueComponent/from-component/src/components/Form.vue
+++ doAll/1vueComponent/from-component/src/components/Form.vue
@@ -16,8 +16,46 @@ export default {
     return {};
   },
 
-  methods: {},
-  created() {},
+  methods: {
+    test() {
+      console.log("测试");
+
+      let p1 = new Promise((resolve, reject) => {
+        resolve({ code: 1 });
+      });
+
+      console.log(p1);
+
+      let login = function() {
+        const p2 = p1.then(res => {
+          // console.log(res);
+          return res;
+        });
+        // console.log(p2);
+        const p3 = p2.then(res => {
+          return "你好";
+          // return new Promise((resolve, reject) => {
+          // resolve("你好");
+          // });
+        });
+        console.log(p3);
+        return p3;
+      };
+
+      // console.log(login());
+      login().then(res => {
+        console.log(res);
+      });
+    },
+    faBu() {
+      let arg = Array.prototype.shift.call(arguments, 3); //删除数组的第一个元素并返回， //改变原数组
+      console.log(arg);
+    }
+  },
+  created() {
+    this.test();
+    this.faBu("name", "age", "你好吗");
+  },
   mounted() {}
 };
 </script>
