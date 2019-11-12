2e867657360ee011950efe0b5979df81ab5db227
libing1 <18317776637@163.com>
2019-11-12 21:47:42 +0800

    home 编译提交

--- doAll/vueCode/compile.js
+++ doAll/vueCode/compile.js
@@ -9,8 +9,27 @@ class Compile {
 
         // 编译
         if (this.$el) {
-            //转换内部内容为
+            //转换内部内容为片段 fragment
+            this.$fragment = this.node2Fragement(this.$el);
+            console.log(this.$fragment);
+
+            //执行编译
+            this.compile(this.$fragment);
+            //将编译完的html结果追加$el
+            this.$el.appendChild(this.$fragment);
         }
+    }
+
+    //将宿主元素中的代码片段拿出来 遍历 ，会比较高效
+    node2Fragement(el) {
+
+        const frag = document.createDocumentFragment();
+        console.log(frag);
+        let child;
+        while ((child = el.firstChild)) {
+            frag.appendChild(child);
+        }
+        return frag
 
     }
 }
