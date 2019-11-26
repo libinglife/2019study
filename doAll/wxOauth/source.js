9c8666e0849ae9b6196ed19d778577721114f78f
libing <18317776637@163.com>
2019-11-26 18:44:54 +0800

    消息接口

--- doAll/wxOauth/source.js
+++ doAll/wxOauth/source.js
@@ -15,11 +15,62 @@ const router = new Router();
 
 app.use(bodyParser());
 app.use(static(__dirname + "/static/"));
+app.use(xmlParser())
 
 // 验证
 router.get('/wechat', cxt => {
     console.log("微信认证...", cxt.url);
-    cxt.body = "你好"
+
+    console.log(url.parse(cxt.url, true));
+
+    const { query } = url.parse(cxt.url, true);
+
+    const {
+        signature, //微信加密签名，signature 结合开发者填写的token 和请求中的timestamp参数 ,nonce参数
+        timestamp, //时间戳
+        nonce, //随机数
+        echostr //随机数
+    } = query;
+    console.log('wechat', query);
+
+    //将 token timestamp nonce 三个参数进行字典排序 并用sha1加密
+
+    let str = [config.token, timestamp, nonce].sort().join('');
+    console.log('str:', str);
+    let strSha1 = crypto.createHash('sha1').update(str).digest('hex');
+    console.log(`自己加密后的字符串为：${strSha1}`);
+
+    //签名对比 相同则按微信要求返回 echostr 参数
+    if (signature === strSha1) {
+        cxt.body = echostr
+    } else {
+        cxt.body = "你不是微信"
+    }
+})
+
+//接受信息
+router.post('/wechat', cxt => {
+
+    console.log("post信息:", cxt);
+
+    const {
+        xml: msg
+    } = cxt.request.body;
+
+    console.log("receive msgs:", msg);
+
+    const builder = new xml2js.Builder();
+    const result = builder.buildObject({
+        xml: {
+            ToUserName: msg.ToUserName,
+            FromUserName: msg.FromUserName,
+            CreateTime: Date.now(),
+            MsgType: msg.MsgType,
+            Content: "消息：hello " + msg.Content
+        }
+    })
+    cxt.body = result;
+
 })
 
 
