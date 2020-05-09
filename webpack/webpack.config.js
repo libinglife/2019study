cd82bd88bd279a3fea93af97da4479fba6f50cae
libing <18317776637@163.com>
2020-05-09 14:53:50 +0800

    webpack 文件修改

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,7 +6,9 @@
  */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
